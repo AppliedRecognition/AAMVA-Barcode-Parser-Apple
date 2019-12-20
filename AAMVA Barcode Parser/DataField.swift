cfb4a0fca4a2a3167fcb2768a3beb9c4124a25ad
Jakub Dolejs <jakubdolejs@gmail.com>
2019-12-20 21:50:14 +0000

    Exposed `DataField` initializer

--- AAMVA Barcode Parser/DataField.swift
+++ AAMVA Barcode Parser/DataField.swift
@@ -8,9 +8,29 @@
 
 import Foundation
 
+/// Representation of an document entry
+/// - Since: 1.0.0
 public struct DataField {
     
+    /// Description (to be shown to the user)
+    /// - Since: 1.0.0
     public let description: String
+    /// Original value as extracted from the barcode
+    /// - Since: 1.0.0
     public let originalValue: String
+    /// Parsed value
+    /// - Since: 1.0.0
     public let parsedValue: String
+    
+    /// Initializer
+    /// - Parameters:
+    ///   - description: Description (to be shown to the user)
+    ///   - originalValue: Original value as extracted from the barcode
+    ///   - parsedValue: Parsed value
+    /// - Since: 1.2.0
+    public init(description: String, originalValue: String, parsedValue: String) {
+        self.description = description
+        self.originalValue = originalValue
+        self.parsedValue = parsedValue
+    }
 }
